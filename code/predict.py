997527545ad078bef7b13258996dcb00cc6ae2f5
Risdan Kristori <75466526+Risdan224@users.noreply.github.com>
2023-12-11 15:21:51 +0700

    Update predict.py

--- code/predict.py
+++ code/predict.py
@@ -4,7 +4,7 @@ import pickle
 def predict(df):
     # Load the model and scaler from the saved file
     # Load model
-    file_name =str(Path(__file__).parents[1] / 'model/used_car_price_model_colab.pkl')
+    file_name =str(Path(__file__).parents[1] / 'model/used_car_price_model_colab_2.pkl')
     model = pickle.load(open(file_name, 'rb'))
     
     pred = model.predict(df)
